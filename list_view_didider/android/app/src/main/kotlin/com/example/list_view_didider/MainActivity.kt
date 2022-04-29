439b30a6bc73bad995017a98c79535deeb4578d2
e.abrosimov <adwardabrosimov@gmail.com>
2022-04-29 22:53:35 +0300

    ListView divider completed

---
+++ list_view_didider/android/app/src/main/kotlin/com/example/list_view_didider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_view_didider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ list_view_didider/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ListView with divider',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const MyHomePage(title: 'ListView with divider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var listData = List.generate(100, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+                child: ListView.separated(
+              itemCount: listData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  color: Colors.purple.withOpacity(index / listData.length),
+                  child: Center(child: Text('Item $index')),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(
+                height: 20,
+                thickness: 5,
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
